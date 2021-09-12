d280dc271a9a2bb4b209fb30c32e3bcd262e66b3
sandeshworld <bansks@uw.edu>
2021-09-12 13:49:31 -0700

    setting up template for subscribing to node and publishing

--- indyRacePack/vehicleControlPublisher.py
+++ indyRacePack/vehicleControlPublisher.py
@@ -2,22 +2,58 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from sensor_msgs.msg import PointCloud2
+
+#topic used to listen to the lidar message
+from 
+
+#topic used to control vehicle - make sure to install these messages to use them
+from lgsvl_msgs.msg import VehicleControlData
+
+# this class will subscribe to the lidar topic and then publish
+# to the vehicle control publisher to maintain follow following going around the track
 
 
 class VehicleControlPublisher(Node):
 
     def __init__(self):
         super().__init__('wall_following')
-        self.publisher_ = self.create_publisher(String, '/lgsvl/vehicle_control_cmd', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.publisher_ = self.create_publisher(VehicleControlData, '/lgsvl/vehicle_control_cmd', 10)
+        # timer_period = 0.5  # seconds
+
+        self.subscription = self.create_subscription(
+            PointCloud2,
+            'lidar_front/points_raw',
+            self.listener_callback,
+            10)
+        
+        self.subscription  # prevent unused variable warning
+
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+    # def timer_callback(self):
+    #     msg = VehicleControlData()
+    #     msg.acceleration_pct = 0.1*0.1*self.i
+    #     msg.braking_pct = 0.0
+    #     msg.target_gear = 1
+    #     self.publisher_.publish(msg)
+    #     self.get_logger().info('Publishing: "%s"' % msg.acceleration_pct)
+    #     self.i += 1
+
+    def returnDistance(self, msg):
+        msg.data
+
+    def listener_callback(self, data):
+
+        distanceFromWall = self.returnDistance(data)
+        
+        msg = VehicleControlData()
+        msg.acceleration_pct = 0.1*0.1*self.i
+        msg.braking_pct = 0.0
+        msg.target_gear = 1
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg.acceleration_pct)
         self.i += 1
 
 
